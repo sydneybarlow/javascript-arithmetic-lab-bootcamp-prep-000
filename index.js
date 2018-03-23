f37d0a3efdd73b6414c73a5594d2b5f5f0673a70
Sydney Barlow <sydneybarlow44@gmail.com>
2018-03-23 13:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ function dec(n) {
   return n-=1
 }
 function makeInt(n) {
-  return parseInt('n', 10)
-}
-function makeInt(n) {
-  return ??????
-}
-function makeInt(n) {
-  return ???????
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
   return ???????
